1200b439b3794b261a2081936575f83002c70d72
risper4 <risperwanjikugichia@gmail.com>
2026-08-01 20:03:41 +0300

    Signup resource

--- server/app.py
+++ server/app.py
@@ -3,11 +3,32 @@ from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 from config import app, api, db
+from models import User, Task
+from schema import UserSchema, TaskSchema
+
+
+class Signup(Resource) :
+    def post(self) :
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User(
+            username=username
+        )
+        user.password_hash = password
+
+        try :
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+        except IntegrityError :
+            return {'error' : '422 Unprocessed Entity'}, 422
 
 
 
-
-
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__' :
     app.run(port=5555, debug=True)
