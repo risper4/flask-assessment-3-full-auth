bdb428f995ed0c4d15af3eacdf30d5eb17342a05
risper4 <risperwanjikugichia@gmail.com>
2026-08-01 11:16:22 +0300

    User table

--- server/models.py
+++ server/models.py
@@ -1,2 +1,16 @@
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
+
+from config import bcrypt, db
+
+
+class User(db.Model) :
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Integer, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+
+    def __repr__(self):
+        return f'<User {self.username}>'
