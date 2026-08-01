a059340d2d2e782c86ccdc48357a015797df323b
risper4 <risperwanjikugichia@gmail.com>
2026-08-01 11:55:38 +0300

    User schema

--- server/schema.py
+++ server/schema.py
@@ -1,1 +1,9 @@
 from marshmallow import Schema, fields
+
+class UserSchema(Schema) :
+    id = fields.Integer()
+    username = fields.String()
+
+    posts = fields.List(fields.Nested(lambda : TaskSchema(exclude=('user',))))
+
+
