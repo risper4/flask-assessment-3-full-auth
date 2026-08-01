89531b9621ed6b19404da6f0ec12172a7ec8aea4
risper4 <risperwanjikugichia@gmail.com>
2026-08-01 11:26:34 +0300

    Password setter

--- server/models.py
+++ server/models.py
@@ -11,6 +11,19 @@ class User(db.Model) :
     username = db.Column(db.Integer, nullable=False, unique=True)
     _password_hash = db.Column(db.String, nullable=False)
 
+    @hybrid_property
+    def password_hash(self) :
+        raise AttributeError('Password hashes are not to be viewed')
+
+    @password_hash.setter
+    def password_hash(self, password) :
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+        
+
 
     def __repr__(self):
         return f'<User {self.username}>'
