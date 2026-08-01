a0b29bff9eb2048e661d48550c60343d67beb520
risper4 <risperwanjikugichia@gmail.com>
2026-08-01 20:37:04 +0300

    Logout resource

--- server/app.py
+++ server/app.py
@@ -56,10 +56,17 @@ class CheckSession(Resource) :
             return {'error' : '401 Unauthorized'}
 
 
+class Logout(Resource) :
+    def post (self) :
+        session['user_id'] = None
+        return {}, 401
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__' :
     app.run(port=5555, debug=True)
